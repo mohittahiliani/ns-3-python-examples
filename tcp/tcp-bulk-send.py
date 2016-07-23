bb6129b685356e167f49e53e015c98dfd0e0d140
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-07-23 09:40:46 +0530

    add copyright to tcp-bulk-send.py

--- tcp/tcp-bulk-send.py
+++ tcp/tcp-bulk-send.py
@@ -1,5 +1,7 @@
 # -*-  Mode: Python; -*-
 # /*
+#  * Copyright (c) 2016 NITK Surathkal
+#  *
 #  * This program is free software; you can redistribute it and/or modify
 #  * it under the terms of the GNU General Public License version 2 as
 #  * published by the Free Software Foundation;
